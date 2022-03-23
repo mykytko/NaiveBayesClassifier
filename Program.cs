9455c602e7625f08a0ae2269bfc8322ab76c7198
mykytko <nd235471@gmail.com>
2022-03-23 20:24:18 +0200

    Refactoring and commentaries

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
+﻿using System.Text;
 
 const string clickbait = "resources/clickbait_data";
 const string nonClickbait = "resources/non_clickbait_data";
